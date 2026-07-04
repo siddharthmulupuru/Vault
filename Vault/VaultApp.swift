99a80407387c17a1e79aa32b821eba8b8a7e5f78
siddharthmulupuru <siddharth100@hotmail.com>
2026-07-04 16:34:29 -0700

    Initial Commit

---
+++ Vault/VaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  VaultApp.swift
+//  Vault
+//
+//  Created by Siddharth Mulupuru on 7/4/26.
+//
+
+import SwiftUI
+
+@main
+struct VaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
